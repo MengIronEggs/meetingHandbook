e2ec26d318af4024b2779d9e8404df58cfc08f5c
mengMengIronEggs <1369665613@qq.com>
2018-07-06 09:19:49 +0800

    全文检索

--- src/view/Management/FullTextSearch.vue
+++ src/view/Management/FullTextSearch.vue
@@ -5,6 +5,12 @@
                 <el-button slot="append" icon="el-icon-search" @click="InputSearchClick"></el-button>
             </el-input>
         </div>
+        <div class="width:100%;">
+            <div :key="index" v-for="(item,index) in searchResultArr">
+                <div>{{item.doc_title}}</div>
+                <div v-html="item.createtime + item.text"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -21,7 +27,10 @@ export default {
         if (this.searchText) {
             // fullsearch?query=主题词
             this.$get(`fullsearch?query=${this.searchText}`).then(res => {
-                console.log(res);
+                // console.log(res);
+                if(res.Code == 200){
+                    this.searchResultArr = res.Data;
+                }
             })
             
         } else {
